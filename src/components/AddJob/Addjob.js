8c9d3db74f8f331111e72ccfedd4d38d94fc84e6
Md. Ibrahim Hossain <ih75754@gmail.com>
2022-11-29 11:42:42 +0600

    first commit

---
+++ src/components/AddJob/Addjob.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Addjob = () => {
+    return (
+        <div>
+            <h1>add jobgfffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff</h1>
+        </div>
+    );
+};
+
+export default Addjob;
---
+++ src/layout/Main.js
@@ -0,0 +1,125 @@
+import MenuIcon from '@mui/icons-material/Menu';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import CssBaseline from '@mui/material/CssBaseline';
+import Drawer from '@mui/material/Drawer';
+import IconButton from '@mui/material/IconButton';
+import Toolbar from '@mui/material/Toolbar';
+import PropTypes from 'prop-types';
+import * as React from 'react';
+
+import { Button, ListItem } from '@mui/material';
+import { Link, Outlet } from 'react-router-dom';
+
+
+const drawerWidth = 250;
+
+function Main(props) {
+    const { window } = props;
+    const [mobileOpen, setMobileOpen] = React.useState(false);
+
+
+    const handleDrawerToggle = () => {
+        setMobileOpen(!mobileOpen);
+    };
+
+    const drawer = (
+        <div style={{ backgroundColor: "#653DF5", height: "100vh", color: "white" }}>
+            <Toolbar >
+                <h1 className="text-bold d-inline custom-font" style={{ marginTop: "15px" }}>Job Portal</h1>
+            </Toolbar>
+
+
+            <Box>
+                <Link to="/home" style={{ color: "white", textDecoration: "none" }}>
+                    <ListItem button className=''>Home</ListItem>
+                </Link>
+
+                <Link to="/addJob" style={{ color: "white", textDecoration: "none" }}>
+                    <ListItem button className=''>Add a job</ListItem>
+                </Link>
+
+                <Link to="" style={{ color: "white", textDecoration: "none" }}>
+                    <ListItem button >Make Admin</ListItem>
+                </Link>
+            </Box>
+        </div>
+    );
+
+    const container = window !== undefined ? () => window().document.body : undefined;
+
+    return (
+        <Box sx={{ display: 'flex' }} >
+            <CssBaseline />
+            <AppBar
+                position="fixed"
+                sx={{
+                    width: { sm: `calc(100% - ${drawerWidth}px)` },
+                    ml: { sm: `${drawerWidth}px` }, backgroundColor: "#653DF5"
+                }}
+            >
+                <Toolbar>
+                    <IconButton
+                        color="inherit"
+                        aria-label="open drawer"
+                        edge="start"
+                        onClick={handleDrawerToggle}
+                        sx={{ mr: 2, display: { sm: 'none' } }}
+                    >
+                        <MenuIcon />
+                    </IconButton>
+
+                    <Link to=""><Button color="inherit" className="w-100 text-white text-decoration-none py-2 mb-2">Dashboard</Button></Link>
+
+                </Toolbar>
+            </AppBar>
+            <Box
+                component="nav"
+                sx={{ width: { sm: drawerWidth }, flexShrink: { sm: 0 } }}
+                aria-label="mailbox folders"
+            >
+                {/* The implementation can be swapped with js to avoid SEO duplication of links. */}
+                <Drawer
+                    container={container}
+                    variant="temporary"
+                    open={mobileOpen}
+                    onClose={handleDrawerToggle}
+                    ModalProps={{
+                        keepMounted: true, // Better open performance on mobile.
+                    }}
+                    sx={{
+                        display: { xs: 'block', sm: 'none' },
+                        '& .MuiDrawer-paper': { boxSizing: 'border-box', width: drawerWidth },
+                    }}
+                >
+                    {drawer}
+                </Drawer>
+                <Drawer
+                    variant="permanent"
+                    sx={{
+                        display: { xs: 'none', sm: 'block' },
+                        '& .MuiDrawer-paper': { boxSizing: 'border-box', width: drawerWidth },
+                    }}
+                    open
+                >
+                    {drawer}
+                </Drawer>
+            </Box>
+            <Box
+                component="main"
+                sx={{ flexGrow: 1, p: 3, width: { sm: `calc(100% - ${drawerWidth}px)` } }}
+            >
+                <Toolbar />
+
+                <Outlet></Outlet>
+
+            </Box>
+        </Box>
+    );
+}
+
+Main.propTypes = {
+    window: PropTypes.func,
+};
+
+export default Main;
